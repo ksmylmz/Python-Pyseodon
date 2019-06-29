63290ba0ac88a862cff17b0c8cccd65503c79878
Kasım YILMAZ <kasim_yilmaz_jw@hotmail.com>
2019-06-29 23:36:42 +0300

    PysSeodon v1
    firsr release

--- Pyseodon.py
+++ Pyseodon.py
@@ -17,30 +17,5 @@ sys.excepthook = catch_exceptions
 
     
 if __name__ == '__main__':
-    
-
-    
-    #headers     
-    headers = { "header_name": "headers_value" }
-    params = {  "param_name": "param_value" }
-    path = ""
-   # path = "https://www.beethoven.com.tr/"
-    
-    #getrequest     
-    #_request = requests.get(path, params=params, headers=headers)
-
-
-
     _controller = controller()
-    #_controller.routes()
 
-
-
-"""
-#path ="https://www.w3schools.com/python/python_dictionaries.asp"
-#path ="https://www.lensoptik.com.tr/"
-#path = "http://www.ufoweb.net"
-#path = "https://www.garantibbva.com.tr/tr"
-path = "https://www.ddfs.com/"
-#path = "https://wp.pts.net/"
-#Content-Encoding"""
